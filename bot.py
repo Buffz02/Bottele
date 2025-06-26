1a91c5809b0d875b16939c2d47df2c92f74ee7c6
Buffz02 <74828223+Buffz02@users.noreply.github.com>
2025-06-26 21:56:23 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,13 @@
 import os
 import telebot
 
-# Prende il token dalla variabile d'ambiente
 TOKEN = os.environ.get("TELEGRAM_TOKEN")
-
 bot = telebot.TeleBot(TOKEN)
 
-# Comando /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Hello world 👋")
 
-# Avvio in polling
+# 👇 Disattiva webhook se attivo
+bot.remove_webhook()
 bot.polling(none_stop=True)
